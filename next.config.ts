256557eca15dcb75b31d954277cd76e92b07a9bf
Zahin Afsar <afsarzahin@gmail.com>
2026-05-07 21:12:30 +0600

    feat: remove basePath and assetPrefix from next.config.ts; add CNAME file for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? "/hippo-ui" : "";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath,
-  assetPrefix: basePath,
   images: { unoptimized: true },
   trailingSlash: true,
 };
